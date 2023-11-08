b3a01e8110b08a1e11d368e6b98e9cda500c58ef
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-09 00:23:55 +0600

    post interface

---
+++ src/app/post/models/post.intestfaces.ts
@@ -0,0 +1,20 @@
+
+export interface IPost {
+    title: string;
+    content: string;
+    authorID: string;
+    publishedDate: Date;
+    comments: IComment[];
+    likes: ILike[];
+  }
+  
+export interface IComment {
+    // Define the properties of your Comment class here
+  }
+  
+  export interface ILike {
+    // Define the properties of your Like class here
+  }
+  
+  
+  
