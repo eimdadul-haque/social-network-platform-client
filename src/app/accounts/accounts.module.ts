cc702655a84a04718c2b18ca090cbc732fc52d8c
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-17 13:27:46 +0600

    lohin and registration page added

---
+++ src/app/accounts/accounts.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { AccountsRoutingModule } from "./accounts-routing.module";
+import { RegistrationComponent } from './registration/registration.component';
+
+@NgModule({
+    declarations:[
+        
+    
+    RegistrationComponent
+  ],
+    imports: [
+        AccountsRoutingModule
+    ],
+    providers: [
+
+    ],
+    exports: [
+
+    ]
+})
+export class AccountsModule {
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'accounts',
+    loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsModule)
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
   },
